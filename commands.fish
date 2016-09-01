5b3d8fe4d802aa9798f01ba70b46ab8202af97ec
Diego Hernandes <diego@hernandev.com>
2016-09-01 17:39:53 -0300

    cleaning up

--- commands.fish
+++ commands.fish
@@ -38,19 +38,16 @@ mkdir -p $A_COMPOSER_CACHE
 function node
 	docker run -it --rm -v (pwd):/var/www/app -v $A_NPM_MOUNT -v $A_SSH_NODE_MOUNT ambientum/node:6 node $argv
 end
-#alias node=node
 
 # NPM
 function npm
 	docker run -it --rm -v (pwd):/var/www/app -v $A_NPM_MOUNT -v $A_SSH_NODE_MOUNT ambientum/node:6 npm $argv
 end
-#alias npm=npm
 
 # Gulp
 function gulp
 	docker run -it --rm -v (pwd):/var/www/app -v $A_NPM_MOUNT -v $A_SSH_NODE_MOUNT ambientum/gulp-cli:1.2 gulp $argv
 end
-#alias gulp=gulp
 
 ####
 # Alias for Composer and other PHP commands
@@ -60,11 +57,9 @@ end
 function php
 	docker run -it --rm -v (pwd):/var/www/app -v $A_COMPOSER_MOUNT -v $A_SSH_PHP_MOUNT ambientum/php:7.0 php $argv
 end
-#alias php=php
 
 
 # Composer
 function composer
 	docker run -it --rm -v (pwd):/var/www/app -v $A_COMPOSER_MOUNT -v $A_SSH_PHP_MOUNT ambientum/php:7.0 composer $argv
 end
-#alias composer=composer
