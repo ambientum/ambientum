a4dfcc6ca579079d6ce888338e1e31ccf5ab2776
Diego Hernandes <diego@hernandev.com>
2016-09-01 19:59:44 -0300

    fix vue

--- commands.bash
+++ commands.bash
@@ -59,7 +59,7 @@ function gulp() {
 alias gulp=gulp
 
 # Vue
-function gulp() {
+function vue() {
 	docker run -it --rm -v $(pwd):/var/www/app -v $A_NPM_CACHE_MOUNT -v $A_NPM_CONFIG_MOUNT -v $A_SSH_NODE_MOUNT ambientum/vue-cli:2.2 vue "$@"
 }
 alias vue=vue
