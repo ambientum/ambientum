c54d6fa14baa73f923e8028d097eecdca4bd405e
Diego Hernandes <diego@hernandev.com>
2016-12-23 06:48:13 -0200

    Apache with php 7.1 bulds

---
+++ php/7.1-apache/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+: "${APACHE_CONFDIR:=/etc/apache2}"
+: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+if test -f "$APACHE_ENVVARS"; then
+	. "$APACHE_ENVVARS"
+fi
+
+# Apache gets grumpy about PID files pre-existing
+: "${APACHE_PID_FILE:=${APACHE_RUN_DIR:=/var/run/apache2}/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
+# Starts FPM
+nohup /usr/sbin/php-fpm7.1 -F -O 2>&1 &
+
+# Starts apache2!
+exec apache2 -DFOREGROUND "$@"
