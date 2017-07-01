5bfda64c208dd4ea29aac4a9527b62cf86c665d1
Diego Hernandes <diego@hernandev.com>
2017-07-01 15:26:46 -0300

    symfony production and development support on php 7.1 - nginx

--- php/7.1-nginx/start.sh
+++ php/7.1-nginx/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Aliasing $FRAMEWORK"
+sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
+
 # Starts FPM
 nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
 
