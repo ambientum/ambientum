c20865b0a7ad24805098844e08d204cf801c92ca
Diego Hernandes <diego@hernandev.com>
2018-01-26 06:49:07 -0200

    php-pm first tests

---
+++ php/7.2-php-pm/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# start php-pm
+/home/ambientum/ppm/vendor/bin/ppm \
+ start \
+ --host=0.0.0.0 \
+ --bootstrap=$FRAMEWORK \
+ --cgi-path=/usr/bin/php-cgi7 \
+ --static-directory=/var/www/app/public \
+ /var/www/app
