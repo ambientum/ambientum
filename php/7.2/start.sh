57da0f64381aa4ca54233476800424fb1ceef6bc
Diego Hernandes <diego@hernandev.com>
2018-03-14 12:41:28 -0300

    updates on PHP 7.2 with xDebug 2.6.0, fixes imagemagick and adds php7-gmp ext

--- php/7.2/start.sh
+++ php/7.2/start.sh
@@ -10,21 +10,21 @@ if [[ $NR_ENABLED == true ]]; then
 	sudo cp /scripts/newrelic.ini /etc/php7/conf.d/00_newrelic.ini
 fi
 
-#if [[ $XDEBUG_ENABLED == true ]]; then
-#    # enable xdebug extension
-#    sudo sed -i "/;zend_extension=xdebug.so/c\zend_extension=xdebug.so" /etc/php7/conf.d/00_xdebug.ini
-#
-#    # enable xdebug remote config
-#    echo "[xdebug]" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.remote_enable=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.remote_port=9000" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.scream=0" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.cli_color=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#    echo 'xdebug.idekey = "ambientum"' | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-#
-#fi
+if [[ $XDEBUG_ENABLED == true ]]; then
+    # enable xdebug extension
+    sudo sed -i "/;zend_extension=xdebug/c\zend_extension=xdebug" /etc/php7/conf.d/00_xdebug.ini
+
+    # enable xdebug remote config
+    echo "[xdebug]" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_enable=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_port=9000" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.scream=0" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.cli_color=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo 'xdebug.idekey = "ambientum"' | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+
+fi
 
 # run the original command
 exec "$@"
