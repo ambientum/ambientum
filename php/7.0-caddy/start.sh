b057644a8444854fe9ba2e7814ecacf5ab967d3d
Diego Hernandes <diego@hernandev.com>
2018-03-14 13:09:58 -0300

    remove caddy, and drop php 7.0 support

--- php/7.0-caddy/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Starts FPM
-nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
-
-# Starts caddy with the default configuration file
-/usr/local/bin/caddy -conf /home/ambientum/Caddyfile
--- php/7.0-nginx/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Starts FPM
-nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
-
-# Starts NGINX!
-nginx
