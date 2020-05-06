18110284061c26c029b172f337a6c70f4fc76b58
Diego Hernandes <iamhernandev@gmail.com>
2020-05-06 15:25:17 -0300

    cert generation

--- php/7.3-nginx/start.sh
+++ php/7.3-nginx/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Aliasing $FRAMEWORK"
+# Regenerate SSL certificate if different than default one.
+if [[ $SSL_DOMAIN != "ambientum.local" ]]; then
+    /bin/mkcert -cert-file=/home/ssl/nginx.crt -key-file=/home/ssl/nginx.key "${SSL_DOMAIN}"
+fi
+
 sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
 # Starts FPM
