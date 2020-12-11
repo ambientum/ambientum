ef822c197e2ad2fe6c49f8e06c98132833a6b6fb
Diego Hernandes <iamhernandev@gmail.com>
2020-12-11 13:56:11 -0300

    drop php 7.2

--- php/7.2-nginx/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
-
-# Starts FPM
-nohup /usr/sbin/php-fpm7 -y /etc/php7/php-fpm.conf -F -O 2>&1 &
-
-# Starts NGINX!
-nginx
