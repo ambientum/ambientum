ce53be8482b884c529d4d90f38b85e4d075dd194
Diego Hernandes <iamhernandev@gmail.com>
2021-11-29 09:39:52 -0300

    node, php, tiny updates

---
+++ php/8.1/scripts/entrypoint.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# source profile always.
+source /etc/profile
+
+# fix home directory permissions.
+sudo chown -R ambientum:ambientum /home/ambientum
+
+# copy bash config into place.
+# cp /home/bashrc /home/ambientum/.bashrc
+
+# run update-cert script.
+bash /scripts/update-cert.sh
+
+# Set PHP memory limit value.
+sudo sed -i "/memory_limit = .*/c\memory_limit = $PHP_MEMORY_LIMIT" /etc/php8/php.ini
+
+# OPCache extreme mode.
+if [[ $OPCACHE_MODE == "extreme" ]]; then
+    # enable extreme caching for OPCache.
+    echo "opcache.enable=1" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.memory_consumption=512" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.interned_strings_buffer=128" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.max_accelerated_files=32531" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.validate_timestamps=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.save_comments=1" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+    echo "opcache.fast_shutdown=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+fi
+
+# OPCache disabled mode.
+if [[ $OPCACHE_MODE == "disabled" ]]; then
+    # disable extension.
+    sudo sed -i "/zend_extension=opcache/c\;zend_extension=opcache" /etc/php8/conf.d/00_opcache.ini
+    # set enabled as zero, case extension still gets loaded (by other extension).
+    echo "opcache.enable=0" | sudo tee -a /etc/php8/conf.d/00_opcache.ini > /dev/null
+fi
+
+if [[ $XDEBUG_ENABLED == true ]]; then
+    # enable xdebug extension
+    sudo sed -i "/;zend_extension=xdebug/c\zend_extension=xdebug" /etc/php8/conf.d/00_xdebug.ini
+
+    # enable xdebug remote config
+    echo "[xdebug]" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_enable=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    # shellcheck disable=SC2006
+    echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_port=9000" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.scream=0" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.cli_color=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo 'xdebug.idekey = "ambientum"' | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+fi
+
+# update ca certificates, without failing.
+sudo update-ca-certificates 2>/dev/null || true
+
+# run the original command
+exec "$@"
