30bb376553f90402144efb2365aad309a72d5620
Michael <michael@jobpixel.com>
2022-02-21 17:46:14 +0000

    updating the start.sh to run the /scripts

--- php/8.1-nginx/start.sh
+++ php/8.1-nginx/start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+if /usr/bin/find '/scripts/' -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v
+then
+	echo '/scripts is not empty attempting to  perform configuration'
+	echo 'looking for shell scripts in /scripts'
+	find '/scripts/' -follow -type f -print | sort -V | while read -r f
+	do
+		case "$f" in
+			*.sh)
+				if [ -x "$f" ];
+				then
+					echo "Launching $f";
+					"$f"
+				else
+					# warn on scripts which are not executasble
+					echo "Ignoring $f, not executable"
+				fi
+				;;
+			*) echo "Ingnoring $f";;
+		esac
+	done
+	echo 'Configuration complete ready for startup'
+else
+	echo 'No files found in /scripts, skipping configuration'
+fi
+
 sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
 # Starts FPM
