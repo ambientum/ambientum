c971d1135f4f80a3d9270a3390a669a0a9988bfd
Diego Hernandes <iamhernandev@gmail.com>
2022-02-22 08:59:35 -0300

    xdebug to use host.docker.internal

--- php/8.0/scripts/entry.d/50-xdebug.sh
+++ php/8.0/scripts/entry.d/50-xdebug.sh
@@ -9,7 +9,7 @@ if [[ $XDEBUG_ENABLED == true ]]; then
     echo "xdebug.remote_enable=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_autostart=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
     # shellcheck disable=SC2006
-    echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_host=host.docker.internal" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_port=9000" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.scream=0" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.cli_color=1" | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
