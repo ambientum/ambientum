ee34bd2159a944474f29f5c3540fb25d0ac3cc23
Diego Hernandes <iamhernandev@gmail.com>
2022-02-22 07:38:23 -0300

    xdebug and server name set

--- php/7.3/scripts/entry.d/50-xdebug.sh
+++ php/7.3/scripts/entry.d/50-xdebug.sh
@@ -7,10 +7,12 @@ if [[ $XDEBUG_ENABLED == true ]]; then
     # enable xdebug remote config
     echo "[xdebug]" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_enable=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo "xdebug.remote_autostart=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    # shellcheck disable=SC2006
     echo "xdebug.remote_host=`/sbin/ip route|awk '/default/ { print $3 }'`" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.remote_port=9000" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.scream=0" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.cli_color=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
     echo "xdebug.show_local_vars=1" | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
-    echo 'xdebug.idekey = "ambientum"' | sudo tee -a /etc/php7/conf.d/00_xdebug.ini > /dev/null
+    echo 'xdebug.idekey = "'${XDEBUG_IDE_KEY:-ambientum}'"' | sudo tee -a /etc/php8/conf.d/00_xdebug.ini > /dev/null
 fi
