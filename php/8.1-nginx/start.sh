07c12f7da824dba656aaf53129cf5115e6e7de5b
Diego Hernandes <iamhernandev@gmail.com>
2022-03-01 16:18:06 -0300

    fix listen mode

--- php/8.1-nginx/start.sh
+++ php/8.1-nginx/start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# alias framework.
 sudo ln -s /etc/nginx/sites/$FRAMEWORK.conf /etc/nginx/sites/enabled.conf
 
-# Starts FPM
-nohup /usr/sbin/php-fpm8 -y /etc/php8/php-fpm.conf -F -O > /dev/null 2>&1 &
-
-# Starts NGINX!
-nginx
+if [[ "${NGINX_LISTEN_MODE}" = "nginx" ]]; then
+  # starts NGINX!
+  nginx
+elif [[ "${NGINX_LISTEN_MODE}" = "fpm" ]]; then
+	# starts FPM
+  /usr/sbin/php-fpm8 -y /etc/php8/php-fpm.conf -F -O
+else
+  # starts FPM
+  nohup /usr/sbin/php-fpm8 -y /etc/php8/php-fpm.conf -F -O > /dev/null 2>&1 &
+  # starts NGINX!
+  nginx
+fi
